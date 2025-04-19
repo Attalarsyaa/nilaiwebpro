d6512dea5a372809be53c4ab7c8aed5d259c86a7
Attalarsyaa <Attalarsyah@gmail.com>
2025-04-19 15:12:51 +0700

    Add Grades Table

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aplikasi Nilai WEB PRO 
+## CRUD Tabel Kelas (GRADES) 
---
+++ home.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+            <h1 class="text-center">Aplikasi Nilai Siswa</h1>
+            <h3 class="text-center">Web Pro</h3>
+        </p>
+    </div>
+</div>
+   
